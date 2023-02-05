3489e3d281b56002199f06504c63f1b08bc3b960
hreinn <hreinngy@gmail.com>
2023-02-05 16:36:59 +0100

    Adds 'isActive' on header Nav items

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,31 @@
 import { NavLink } from "react-router-dom";
 import "../App.css";
 
+function setNavItemActive({ isActive }) {
+  return {
+    textDecoration: isActive ? "underline" : "inherit",
+  };
+}
+
 export default function NavBar() {
   return (
     <header>
       <nav>
         <ul className="nav-menu">
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/" style={setNavItemActive}>
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/contact" style={setNavItemActive}>
+              Contact
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/about">About</NavLink>
+            <NavLink to="/about" style={setNavItemActive}>
+              About
+            </NavLink>
           </li>
         </ul>
       </nav>
