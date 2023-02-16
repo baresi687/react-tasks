1164deab2e5a82a48d05f0a5dbeaf9c13915adcb
hreinn <hreinngy@gmail.com>
2023-02-16 11:29:42 +0100

    Adds react-hook-form and react-select packages. Adds the required inputs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import { useState } from "react";
 import "./App.css";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
+
+const skillsOptions = [
+  { value: "html", label: "HTML" },
+  { value: "css", label: "CSS" },
+  { value: "javascript", label: "JavaScript" },
+];
 
 function App() {
-  return <h1>Hello</h1>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <>
+      <h1>React Hook Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Name" {...register("name")} />
+        <input type="email" placeholder="Email" {...register("email")} />
+        <input type="password" placeholder="Password" {...register("password")} />
+        <input type="password" placeholder="Confirm Password" {...register("password-confirm")} />
+        <Controller
+          name="skills"
+          control={control}
+          render={({ field }) => (
+            <Select
+              isMulti
+              options={skillsOptions}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              {...field}
+            />
+          )}
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
