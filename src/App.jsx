3664b1f2dc87844f5f3fc8b4726c5494d8cb62f5
hreinn <hreinngy@gmail.com>
2023-02-16 13:25:41 +0100

    Adds yup with form validation rules. Add labels to inputs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,70 @@
 import "./App.css";
 import { useForm, Controller } from "react-hook-form";
 import Select from "react-select";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-const skillsOptions = [
-  { value: "html", label: "HTML" },
-  { value: "css", label: "CSS" },
-  { value: "javascript", label: "JavaScript" },
-];
+const schema = yup.object({
+  name: yup.string().required("Please enter your name").min(4, "Name must be 4 characters or more"),
+  email: yup.string().required("Please enter your email address").email("Please enter a valid email address"),
+  password: yup.string().required("Please enter a password").min(8, "Password must be 8 characters or more"),
+  confirmPassword: yup
+    .string()
+    .required("Please confirm the password")
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+});
 
 function App() {
   const {
     register,
     handleSubmit,
-    watch,
     control,
     formState: { errors },
-  } = useForm();
+  } = useForm({ resolver: yupResolver(schema) });
 
   const onSubmit = (data) => console.log(data);
 
   return (
     <>
-      <h1>React Hook Form</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="Name" {...register("name")} />
-        <input type="email" placeholder="Email" {...register("email")} />
-        <input type="password" placeholder="Password" {...register("password")} />
-        <input type="password" placeholder="Confirm Password" {...register("password-confirm")} />
-        <Controller
-          name="skills"
-          control={control}
-          render={({ field }) => (
-            <Select
-              isMulti
-              options={skillsOptions}
-              className="basic-multi-select"
-              classNamePrefix="select"
-              {...field}
-            />
-          )}
-        />
+        <h1>React Hook Form</h1>
+        <label>
+          <input type="text" placeholder="Your name" {...register("name", { required: true })} />
+          {errors.name && <span className="input-error">{errors.name.message}</span>}
+        </label>
+        <label>
+          <input type="email" placeholder="Email" {...register("email", { required: true })} />
+          {errors.email && <span className="input-error">{errors.email.message}</span>}
+        </label>
+        <label>
+          <input type="password" placeholder="Password" {...register("password", { required: true })} />
+          {errors.password && <span className="input-error">{errors.password.message}</span>}
+        </label>
+        <label>
+          <input type="password" placeholder="Confirm Password" {...register("confirmPassword", { required: true })} />
+          {errors.confirmPassword && <span className="input-error">{errors.confirmPassword.message}</span>}
+        </label>
+        <h2 className="skills-heading">Select skills</h2>
+        <label>
+          <Controller
+            name="skills"
+            control={control}
+            render={({ field }) => (
+              <Select
+                id="skills"
+                isMulti
+                options={[
+                  { value: "react", label: "React" },
+                  { value: "angular", label: "Angular" },
+                  { value: "typescript", label: "TypeScript" },
+                ]}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                {...field}
+              />
+            )}
+          />
+        </label>
         <button>Submit</button>
       </form>
     </>
